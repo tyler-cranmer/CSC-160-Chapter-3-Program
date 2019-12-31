5405663f5d57ed9e38d2cbf6c4c77c2aa4a6f5e7
Tyler Cranmer <tyler.cranmer@outlook.com>
2019-12-30 17:15:48 -0700

    CSC 160 Chapter 3 Program

--- CSC 160 Chapter 3 Program/CSC 160 Chapter 3 Program.cpp
+++ CSC 160 Chapter 3 Program/CSC 160 Chapter 3 Program.cpp
@@ -1,20 +1,38 @@
 // CSC 160 Chapter 3 Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Program takes in the amount of tickets sold and their associated amounts. Calculates the total number of tickets sold and total sale amount of the tickets. 
+//Sends the data to a second file for storage and then outputs the results.
 
+// header files 
+#include <fstream> 
 #include <iostream>
+#include <iomanip> 
 
-int main()
+using namespace std; // allows to use cout, endl without prefix std: :
+
+int main() // Main Program
 {
-    std::cout << "Hello World!\n";
+	ofstream outFile; // declare output file stream variable
+
+	int box_tickets_sold = 5750; // declare variables
+	int sideline_tickets_sold = 28000;
+	int premium_tickets_sold = 35750;
+	int general_ticket_sold = 18750;
+
+	double box_amount = 250.00;
+	double sideline_amount = 100.00;
+	double premium_amount = 50.00;
+	double general_amount = 25.00;
+
+	outFile.open("outText.txt"); // associate file stream variables with output sources
+
+	// total tickets sold and total sale calculations
+	int totalTicketsSold = box_tickets_sold + sideline_tickets_sold + premium_tickets_sold + general_ticket_sold;
+	double totalSale = (box_tickets_sold * box_amount) + (sideline_tickets_sold * sideline_amount) + (premium_tickets_sold * premium_amount) + (general_ticket_sold * general_amount);
+
+	cout << fixed << showpoint << setprecision(2); // set manipulator floating-point output to two decimal places
+	cout << "Number of tickets sold = " << totalTicketsSold << endl; // output to file
+	cout << "Sale amount = $" << totalSale << endl; //output to file
+
+	outFile.close(); //close file
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
